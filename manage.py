d86bc6b0428f1fee862a07a8232db40b86f691ea
Christopher Covington <cov@mykolab.com>
2024-03-01 11:46:52 -0500

    ALLOWEDFLARE_EMAIL_DOMAIN

--- manage.py
+++ manage.py
@@ -5,14 +5,14 @@ import sys
 import default
 
 
-def main():
+def main() -> None:
     """Run administrative tasks."""
     try:
         from django.core.management import execute_from_command_line
-        from django.core.management.commands.runserver import Command as runserver
+        from django.core.management.commands.runserver import Command
 
-        runserver.default_addr = '0.0.0.0'
-        runserver.default_port = '8001'  # Should match docker-compose.yml
+        Command.default_addr = '0.0.0.0'  # noqa: S104
+        Command.default_port = '8001'  # Should match docker-compose.yml
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
