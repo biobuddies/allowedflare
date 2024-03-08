593d5b6f7f1eb0e753f81a5b626bb54d7783de6b
Christopher Covington <cov@mykolab.com>
2024-03-08 11:40:01 -0500

    Workers proxy

---
+++ allowedflare-proxy/index.test.ts
@@ -0,0 +1,31 @@
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest';
+import { unstable_dev } from 'wrangler';
+import type { UnstableDevWorker } from 'wrangler';
+
+global.fetch = vi.fn()
+
+function fetchResponse(data) {
+    return {
+        json: () => new Promise((resolve) => resolve(data))
+    }
+}
+
+describe('Worker', () => {
+	let worker: UnstableDevWorker
+
+	beforeAll(async () => {
+		worker = await unstable_dev('allowedflare-proxy/index.ts', {
+			experimental: { disableExperimentalWarning: true },
+		})
+	})
+
+	afterAll(async () => {
+		await worker.stop()
+	})
+
+	it('should fetch the subdomain', async () => {
+        fetch.mockResolvedValue(fetchResponse({}))
+		await worker.fetch('https://subdomain.example.com/x/subbysub/path?query=string')
+        expect(fetch).toHaveBeenCalledWith('https://subbysub.example.com/path?query=string')
+	})
+})
