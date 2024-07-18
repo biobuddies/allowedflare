981226c89c89adb8a35f4877cfa6fd4b98c9b0af
Christopher Covington <cov@mykolab.com>
2024-07-18 09:30:27 -0400

    unskip test

--- allowedflare/test_django.py
+++ allowedflare/test_django.py
@@ -1,11 +1,9 @@
 from django.test import RequestFactory
-from pytest import mark
 
 from allowedflare import LoginView
 
 
-@mark.skip(reason="DJANGO_SETTINGS_MODULE / Apps aren't loaded yet problems")
-def test_allowedflare_login_view(rf: RequestFactory, django_settings_module):
+def test_allowedflare_login_view(rf: RequestFactory):
     response = LoginView.as_view()(rf.get(''))
     assert response.status_code == 200
     assert (
