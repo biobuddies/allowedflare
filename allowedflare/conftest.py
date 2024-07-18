9b193b54ad8a5fe8dfefa70d27e1d0a4671dc3b3
Christopher Covington <cov@mykolab.com>
2024-07-18 09:29:08 -0400

    DJANGO_SETTINGS_MODULE

--- allowedflare/conftest.py
+++
@@ -1,9 +0,0 @@
-from pytest import fixture
-
-from default import settings_module
-
-
-@fixture(autouse=True)
-def django_settings_module(monkeypatch):
-    print(*settings_module.args)
-    monkeypatch.setenv(*settings_module.args)
